44437753d338dd9d0f1af5434d5a65a55ac2cf25
Eleanor Quint <pquint@cse.unl.edu>
2020-01-07 10:29:20 -0600

    Update cpvae.py
    cpvae.cpvae:
    - Add docs to a few of the functions
    - Change how sample works: rather than taking a `z` value, takes a bool controlling whether the default prior or class-based prior should be used in sampling `z`

--- pyroclast/cpvae/cpvae.py
+++ pyroclast/cpvae/cpvae.py
@@ -20,20 +20,32 @@ class CpVAE(tf.Module):
                  box_num,
                  output_dist,
                  name='cpvae'):
+        """Builds a CpVAE TF Module which performs generation and classification
+
+        Args:
+            encoder (Module): function from input data to loc and scale tensors each of length equal to `latent_dimension`
+            decoder (Module): function from latent variable to loc and scale tensors of shape equal to the input data
+            decision_tree (sklearn.tree.DecisionTreeClassifier):
+            latent_dimension (int): number of dimensions in the latent variable
+            class_num (int): number of classes to classify data into
+            box_num (int): maximum number of boxes in the decision tree
+            output_dist (str): name of the distribution to use as the VAE output, mostly used to calculate distortion
+        """
         super(CpVAE, self).__init__(name=name)
         self.encoder = encoder
         self.decoder = decoder
         self.decision_tree = decision_tree
 
-        # tree_stuff
+        # tree_stuff, these must be set before classification is attempted
         self.lower = None
         self.upper = None
         self.values = None
 
-        # multi-gaussian prior
+        # Set a default prior of the standard (0,I) Gaussian
         self.default_prior = tfp.distributions.MultivariateNormalDiag(
             loc=tf.zeros(latent_dimension),
             scale_diag=tf.ones(latent_dimension))
+        # If the prior is per-class, use a Gaussian with learned parameters for each
         self.class_priors = [
             tfp.distributions.MultivariateNormalDiag(
                 loc=tf.Variable(np.zeros(latent_dimension, dtype=np.float32),
@@ -63,11 +75,15 @@ class CpVAE(tf.Module):
             exit()
 
     def __call__(self, x, y=None):
+        # autoencode
         loc, scale_diag = self._encode(x)
         z_posterior = tfp.distributions.MultivariateNormalDiag(
             loc=loc, scale_diag=scale_diag)
         z = z_posterior.sample()
         x_hat, x_hat_scale = self._decode(z)
+        # classification
+        assert not (self.lower is None or self.upper is None or
+                    self.values is None)
         y_hat = transductive_box_inference(loc, scale_diag, self.lower,
                                            self.upper, self.values)
         return x_hat, y_hat, z_posterior, x_hat_scale
@@ -79,9 +95,19 @@ class CpVAE(tf.Module):
     def _decode(self, z):
         return self.decoder(z)
 
-    def sample(self, sample_shape=(1), z=None):
-        if z is None:
-            z = self.default_prior.sample(sample_shape)
+    def sample(self, sample_num=1, use_class_prior=False):
+        """Sample from the generative distribution
+
+        Args:
+            sample_shape (int): Number of samples
+            use_class_prior (bool):
+        """
+        if use_class_prior:
+            class_choices = tfp.distributions.Categorical(
+                [1 / self.class_num] * self.class_num).sample(sample_num)
+            z = np.array([self.class_priors[c].sample() for c in class_choices])
+        else:
+            z = self.default_prior.sample(sample_num)
         return self._decode(z)
 
     def vae_loss(self, x, x_hat, x_hat_scale, z_posterior, y=None):
