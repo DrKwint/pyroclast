c9fabcd4672b4699260aecb260a71e6ad2d71645
Eleanor Quint <equint@cse.unl.edu>
2020-02-14 13:48:39 -0600

    Add ability to register custom datasets and finally rename eager_run -> run (#52)
    * Delete the old run.py to rename eager_run to run
    
    * Add ability to get custom datasets
    
    * Clarified some docs in common.datasets

---
+++ pyroclast/common/datasets.py
@@ -0,0 +1,46 @@
+mapping = {}
+
+
+def register(name):
+
+    def _thunk(func):
+        mapping[name] = func
+        return func
+
+    return _thunk
+
+
+# Implement datasets here
+
+
+def get_dataset_builder(name):
+    """
+    Args:
+        name (str): name under which a dataset has been registered
+
+    Returns:
+        data_dict_fn (dict): a function which takes kwargs and returns a data dictionary
+
+    Notes:
+        If you want to register your own dataset outside datasets.py, you can importy
+        the `register` decorator and use it identically to how it's used in this file.
+        Then, you can get the dataset from anywhere by importing this function.
+
+    Usage Example:
+    -------------
+    from pyroclast.common.datasets import register
+    @register("your_dataset_name")
+    def your_dataset_define(**ds_kwargs):
+        ...
+        return dataset_fn
+    """
+    if callable(name):
+        return name
+    elif name in mapping:
+        return mapping[name]
+    else:
+        raise ValueError('Unknown dataset: {}'.format(name))
+
+
+def check_datasets(name):
+    return name in mapping.keys()
