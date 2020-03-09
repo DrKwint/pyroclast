518f5f865f385b0ff24f64671cb6bddee51bbf6a
Eleanor Quint <pquint@cse.unl.edu>
2020-03-09 13:02:30 -0500

    update attack net

--- pyroclast/features/networks.py
+++ pyroclast/features/networks.py
@@ -24,11 +24,15 @@ def ross_net():
 def tiny_net():
     return tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, 5, padding='same'),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU(),
         tf.keras.layers.Conv2D(64, 5, padding='same'),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU(),
         tf.keras.layers.MaxPool2D(padding='same'),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(64),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU()
     ])
